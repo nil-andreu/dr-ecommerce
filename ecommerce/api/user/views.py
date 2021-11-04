8e6d204db2d06b015c39de6c569a749af5249744
Nil Andreu <nilandreug@gmail.com>
2021-11-04 21:55:40 +0100

    re

--- ecommerce/api/user/views.py
+++ ecommerce/api/user/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+# FOR REGEX
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from .serializers import UserSerializer
+from .models import CustomUser
+from django.http import JsonResponse
+from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_exempt # As we will want to make a change by ourselves of the content, so we do not apply intentionally CSRF token
+from django.contrib.auth import login, logout # For default login and logout
+import re # To run the Regular Expression
 
+# FOR TOKEN
 import random
-# Create your views here.
-
 # Define the method for generating token
 def generate_session_token(length=20):
     return ''.join(random.SystemRandom().choice([chr(i) for i in range(97, 123)] + [str(i) for i in range(10)]) for _ in range(length))
@@ -17,3 +25,36 @@ def generate_session_token(length=20):
     random.SystemRandom().choice(list1+list2+list3+list4) for _ in range(15)
     
     '''
+
+# Some body is going to make a request to the signin
+@csrf_exempt
+def signin(request):
+    # We identify whether it is a post or get
+    if not request.method is "POST":
+        return JsonResponse({'error':'Send a post request with valid parameter only'})
+
+    # We will grap the information of the post method made to sign up
+    username = request.POST["email"] 
+    password = request.POST["password"]
+
+    # VALIDATION PART
+    # We do some of the sanitasation, with re.match() to match the pattern of the regular expression and return True if matches
+    if not re.match("/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g", username):
+        return JsonResponse({'error':'Enter a valid email'})
+    
+    # We could also check for the password (uppercase, lowercase, ...)
+    if not len(password) > 3 :
+        return JsonResponse({"error":"Enter a password of at least 3 characters"})
+    
+    # ONCE VALIDATION IS PASSED, NOW IS SECURE TO USE USERNAME AND PASSWORD
+    UserModel = get_user_model() # I am grabbing a user model
+    # Now i will try to grab the user from the database and match its password
+    # As we are making a request from another origin, we must apply csrf_exempt --> applied just before this view
+    try:
+        pass
+    except UserModel.DoesNotExist: # In the case it does not exist
+        return JsonResponse({"error":"Invalid email"})
+
+
+
+
