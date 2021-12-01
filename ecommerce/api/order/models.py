c8e6a688194f8d02ff8344b3e0210c6a7ba4fa7f
Nil Andreu <nilandreug@gmail.com>
2021-12-01 22:03:17 +0100

    Creating the model

--- ecommerce/api/order/models.py
+++ ecommerce/api/order/models.py
@@ -9,3 +9,19 @@ from api.product.models import Product
 
 # Create your models here.
 class Order(models.Model):
+    # We are going to bring the user
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    product_names = models.CharField(max_length=500)
+    total_products = models.CharField(max_length=500, default=0)
+    total_amount = models.CharField(max_length=50, default=0)
+
+
+    # We might want a transaction id that will be needed later for validating transactions
+    transaction_id = models.CharField(max_length=200, default=0)
+
+
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+
